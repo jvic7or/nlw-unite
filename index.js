9d9e88b84fb2c74fcfd9604f685dd09c3926504d
jvic7or <142541353+jvic7or@users.noreply.github.com>
2024-04-05 14:10:49 -0300

    Add files via upload
    Versão final do projeto feito na NLW Unite para estudo de HTML, CSS e JavaScript

---
+++ index.js
@@ -0,0 +1,147 @@
+let participantes = [
+  {
+    nome: "João Victor",
+    email: "joaoV@gmail.com",
+    dataInscricao: new Date(2024, 3, 1, 19, 20),
+    dataCheckIn: new Date(2024, 3, 2, 18, 00)
+  },
+  {
+    nome: "Pedro Dedoni",
+    email: "pedroD@gmail.com",
+    dataInscricao: new Date(2024, 2, 19, 17, 25),
+    dataCheckIn: null
+  },
+  {
+    nome: "Keven Maximus",
+    email: "kevenM@gmail.com",
+    dataInscricao: new Date(2024, 2, 18, 22, 05),
+    dataCheckIn: new Date(2024, 2, 20, 15, 00)
+  },
+  {
+    nome: "Fabio Barbosa",
+    email: "fabioB@gmail.com",
+    dataInscricao: new Date(2024, 2, 26, 23, 48),
+    dataCheckIn: null
+  },
+  {
+    nome: "Daiane Conceição",
+    email: "annieC@gmail.com",
+    dataInscricao: new Date(2024, 3, 1, 19, 20),
+    dataCheckIn: new Date(2024, 3, 2, 19, 00)
+  },
+  {
+    nome: "Gabriel Sena",
+    email: "gabrielS@gmail.com",
+    dataInscricao: new Date(2024, 2, 15, 09, 30),
+    dataCheckIn: new Date(2024, 2, 20, 18, 00)
+  },
+  {
+    nome: "Felipe Sadrak",
+    email: "felipeS@gmail.com",
+    dataInscricao: new Date(2024, 3, 1, 19, 20),
+    dataCheckIn: null
+  },
+  {
+    nome: "Pedro Coiado",
+    email: "pedroC@gmail.com",
+    dataInscricao: new Date(2024, 2, 1, 21, 10),
+    dataCheckIn: null
+  },
+  {
+    nome: "Vitória Passos",
+    email: "vitoriaP@gmail.com",
+    dataInscricao: new Date(2024, 2, 1, 10, 40),
+    dataCheckIn: new Date(2024, 2, 2, 11, 50)
+  },
+]
+
+const criarNovoParticipante = (participante) => {
+  const dataInscricao = dayjs(Date.now()).to(participante.dataInscricao)
+  let dataCheckIn = dayjs(Date.now()).to(participante.dataCheckIn)
+
+  //condicional
+  if (participante.dataCheckIn == null){
+    dataCheckIn = `
+      <button
+        data-email="${participante.email}"
+        onclick="fazerCheckIn(event)"
+      >
+        Confirmar check-in
+      </button>
+    `
+  }
+
+  return `
+  <tr>
+    <td>
+      <strong>
+        ${participante.nome}
+      </strong>
+      <br>
+      <small>
+        ${participante.email}
+      </small>
+    </td>
+    <td>${dataInscricao}</td>
+    <td>${dataCheckIn}</td>
+  </tr>
+  `
+}
+
+const atualizarLista = (participantes) => {
+  let output = ""
+  //estrutura de repetição - loop
+  for(let participante of participantes){
+    output = output + criarNovoParticipante(participante)
+  }
+
+  //substituir informação do HTML
+  document
+  .querySelector('tbody')
+  .innerHTML = output
+} //arrow Function
+
+atualizarLista(participantes)
+
+const adicionarParticipante = (event) => {
+  event.preventDefault()
+
+  const dadosDoFormulario = new FormData(event.target)
+
+  const participante = {
+    nome: dadosDoFormulario.get('nome'),
+    email: dadosDoFormulario.get('email'),
+    dataInscricao: new Date(),
+    dataCheckIn: null
+  }
+
+  const participanteExiste = participantes.find(
+    (p) => p.email == participante.email
+  )
+
+  if(participanteExiste) {
+    alert('E-mail já cadastrado!')
+    return
+  }
+
+  participantes = [participante, ...participantes]
+  atualizarLista(participantes)
+
+  event.target.querySelector('[name="nome"]').value = ""
+  event.target.querySelector('[name="email"]').value = ""
+}
+
+const fazerCheckIn = (event) => {
+  const mensagemConfirmacao = 'Tem certeza que deseja fazer o check-in?'
+  if(confirm(mensagemConfirmacao) == false){
+    return
+  }
+
+  const participante = participantes.find((p) => {
+    return p.email == event.target.dataset.email
+  })
+
+  participante.dataCheckIn = new Date()
+
+  atualizarLista(participantes)
+}
---
+++ index.md
@@ -0,0 +1,56 @@
+#HTML
+*Hypertext*
+
+
+*Markups*
+ - Tag
+ - Atributos
+
+*Language*
+
+
+#CSS
+/* Cascating StyleSheet */
+```css 
+/*Declarações*/
+body {
+  background-color: #121214;
+  color: white
+}
+```
+
+#JavaScript
+```js 
+//variáveis
+const mensagem = 'Oi, tudo bem?'
+
+//tipos de dados
+  // number
+  // string
+
+//funções
+alert(mensagem)
+
+//objeto javascript
+const participante = {
+  nome: "João Victor",
+  email: "joao@gmail.com",
+  dataInscricao: new Date(2024, 5, 1, 19, 20),
+  dataCheckIn: new Date(2024, 5, 2, 18, 00)
+}
+//array
+let participantes = [
+  {
+    nome: "João Victor",
+    email: "joao@gmail.com",
+    dataInscricao: new Date(2024, 5, 1, 19, 20),
+    dataCheckIn: new Date(2024, 5, 2, 18, 00)
+  }
+]
+
+  //estrutura de repetição - loop
+  for(let participante of participantes){
+    //faça alguma coisa enquanto houver um participante em participantes
+  }
+
+```
